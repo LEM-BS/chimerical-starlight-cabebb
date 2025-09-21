eab022471e1a5cd4fc772c5621befb7923a43369
LEM-BS <lbutler@lembuildingsurveying.co.uk>
2025-09-21 11:41:02 +0100

    Add quote calculator page and interactive widget

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://www.lembuildingsurveying.co.uk',
   output: 'static',
+  integrations: [react()],
   build: {
     format: 'file'
   },
