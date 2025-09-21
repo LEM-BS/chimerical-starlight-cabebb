36186cee95f8568252cc269c40574b82d5933189
LEM-BS <lbutler@lembuildingsurveying.co.uk>
2025-09-21 13:08:20 +0100

    Normalize full postcodes before area lookup

---
+++ src/lib/areas.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest';
+
+import { getAreasForOutcode } from './areas';
+
+describe('getAreasForOutcode', () => {
+  it('returns expected areas for CH5 outcode', () => {
+    const areas = getAreasForOutcode('CH5');
+    expect(areas.map((area) => area.id)).toEqual([
+      'connahs-quay',
+      'shotton',
+      'queensferry',
+      'hawarden',
+      'ewloe',
+      'deeside',
+    ]);
+  });
+
+  it('normalises full postcodes to match their outcodes', () => {
+    const fromOutcode = getAreasForOutcode('CH5');
+    const fromPostcode = getAreasForOutcode('CH5 4HS');
+
+    expect(fromPostcode.map((area) => area.id)).toEqual(fromOutcode.map((area) => area.id));
+  });
+});
